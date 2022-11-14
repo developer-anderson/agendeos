3e319f023d3185f3e623fc69fa7840b1e8daf7ef
Anderson NAscimento <anderson.asnascimentto@gmail.com>
2022-11-14 09:56:13 -0300

    Configuração de ambientes e iniciando o modelo

---
+++ app/Http/Controllers/OrdemServicosController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrdemServicos;
+use Illuminate\Http\Request;
+
+class OrdemServicosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\OrdemServicos  $ordemServicos
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OrdemServicos $ordemServicos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\OrdemServicos  $ordemServicos
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OrdemServicos $ordemServicos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\OrdemServicos  $ordemServicos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OrdemServicos $ordemServicos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\OrdemServicos  $ordemServicos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OrdemServicos $ordemServicos)
+    {
+        //
+    }
+}

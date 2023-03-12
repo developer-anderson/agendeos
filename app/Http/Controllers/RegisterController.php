893cee0c32581f22fb94cc2fa4c5ac0d6c6f8a09
developer-anderson <anderson.asnascimentto@gmail.com>
2023-03-12 07:12:58 -0300

    inclusão de validações

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Password;
 // use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 
@@ -12,17 +15,26 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        $attributes = request()->validate([
-            'username' => 'required|max:255|min:2',
-            'email' => 'required|email|max:255|unique:users,email',
-            'password' => 'required|min:5|max:255',
-            'terms' => 'required'
-        ]);
-        $user = User::create($attributes);
-        auth()->login($user);
-
+  
+        
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) 
+        {
+            return response()->json(["error" => "true", "msg" => "O e-mail informado está associado a outra conta"],500);
+        }
+        else
+        {
+            $attributes = request()->validate([
+                'username' => 'required|max:255|min:2',
+                'email' => 'required|email|max:255|unique:users,email',
+                'password' => 'required|min:5|max:255'
+            ]);
+            
+            $user = User::create($attributes);
+            auth()->login($user);
+            return response()->json(Auth::id(), 200);
+        }
         return redirect('/dashboard');
     }
 }

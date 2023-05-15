fde1b0c00810d732dde5fbbaea4fcc1ff474c61f
developer-anderson <anderson.asnascimentto@gmail.com>
2023-05-15 20:57:48 -0300

    inclusão de dados do usuário

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Password;
-
+use App\Models\fluxo_caixa;
+use App\Models\User;
 class LoginController extends Controller
 {
     /**
@@ -28,12 +29,14 @@ class LoginController extends Controller
 
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
             $request->session()->regenerate();
-
-            return response()->json(Auth::id(), 200);
+            $vetor  = User::find(Auth::id());
+            //$vetor['receita'] = fluxo_caixa::getAllMoney(Auth::id());
+            return response()->json($vetor, 200);
         }
         else{
             return response()->json(["error" => "true", "msg" => "Dados inválidos"],404);
         }
+
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
         ]);

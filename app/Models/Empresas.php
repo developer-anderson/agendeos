8382049dae6423357be512a3bbc965b0a82d7651
Anderson Nascimento <anderson.asnascimentto@gmail.com>
2023-12-11 18:28:05 -0300

    Inclusao de horarios da empresa

--- app/Models/Empresas.php
+++ app/Models/Empresas.php
@@ -9,7 +9,17 @@ class Empresas extends Model
 {
     use HasFactory;
     protected $table = 'empresas';
-    protected $fillable =
-    ['razao_social', 'cnpj', 'situacao', 'plano_id', 'segmento_id',  'telefone',
-     'cep', 'logradouro', 'numero', 'complemento', 'bairro', 'estado', 'cidade', 'use_terms'];
+    protected $fillable = [
+        'razao_social', 'cnpj', 'situacao', 'plano_id', 'segmento_id', 'telefone',
+        'cep', 'logradouro', 'numero', 'complemento', 'bairro', 'estado', 'cidade',
+        'use_terms',
+        'segunda_horario_inicio', 'segunda_horario_fim',
+        'terca_horario_inicio', 'terca_horario_fim',
+        'quarta_horario_inicio', 'quarta_horario_fim',
+        'quinta_horario_inicio', 'quinta_horario_fim',
+        'sexta_horario_inicio', 'sexta_horario_fim',
+        'sabado_horario_inicio', 'sabado_horario_fim',
+        'domingo_horario_inicio', 'domingo_horario_fim',
+        "segunda", "terca", "quarta", "quinta","sexta","sabado", "domingo"
+    ];
 }

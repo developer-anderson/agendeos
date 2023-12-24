3f90ed02e3516f3b9ec5c60d0ecda61fe65c405d
Anderson Nascimento <anderson.asnascimentto@gmail.com>
2023-12-24 10:26:34 -0300

    ajustes

--- routes/web.php
+++ routes/web.php
@@ -25,16 +25,16 @@ use App\Http\Controllers\OrdemServicosController;
 use App\Http\Controllers\FluxoCaixaController;
 use App\Http\Controllers\AgendamentoController;
 use App\Http\Controllers\UploadController;
-Route::get('agendamento/getEstabelecimento/{slug}', [OrdemServicosController::class, 'getEstabelecimento'])->name('getEstabelecimento');
+Route::get('agendamento/getEstabelecimento/{slug}', [OrdemServicosController::class, 'getEstabelecimento'])->name('getEstabelecimentoWeb');
 
 Route::permanentRedirect('/', 'https://site.agendos.com.br/');
 
-Route::post('/cadastrar', [App\Http\Controllers\RegisterController::class, 'store'])->name('cadastrar');
-Route::post('/reset-password-token', [App\Http\Controllers\PasswordResetController::class, 'sendResetLinkEmail'])->name('token_recuperar_Senha');
+Route::post('/cadastrar', [App\Http\Controllers\RegisterController::class, 'store'])->name('cadastrarWeb');
+Route::post('/reset-password-token', [App\Http\Controllers\PasswordResetController::class, 'sendResetLinkEmail'])->name('token_recuperar_SenhaWeb');
 Route::post('/password/reset', [App\Http\Controllers\PasswordResetController::class, 'resetPassword'])->name('password.reset.form');
 
-Route::post('/reset-password', [App\Http\Controllers\PasswordResetController::class, 'resetPassword'])->name('trocar_senha');
-Route::post('logout', [App\Http\Controllers\LoginController::class, 'logout'])->name('logout');
+Route::post('/reset-password', [App\Http\Controllers\PasswordResetController::class, 'resetPassword'])->name('trocar_senhaWeb');
+Route::post('logout', [App\Http\Controllers\LoginController::class, 'logout'])->name('logoutWeb');
 Route::get('/politica-de-privacidade', function () {
     return view('politica-privacidade');
 });

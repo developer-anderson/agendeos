36a7804dfe4ed13522e3bd63d323d7f2a0fccd89
Anderson Nascimento <anderson.asnascimentto@gmail.com>
2024-01-14 09:39:36 -0300

    Ajustes

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\UsuarioAssinatura;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Support\Facades\Password;
@@ -10,11 +12,6 @@ use App\Models\User;
 use App\Models\fluxo_caixa;
 class RegisterController extends Controller
 {
-    public function show()
-    {
-        return view('auth.register');
-    }
-
     public function store(Request $request)
     {
         $data = $request->all();
@@ -26,17 +23,21 @@ class RegisterController extends Controller
         else
         {
             $data['password'] = bcrypt( $data['password']);
+            $dataAtual = Carbon::now();
+            $dataFutura = $dataAtual->addDays(30);
+            $dataFuturaFormatada = $dataFutura->format('Y-m-d');
             $user = User::create($data);
-            Auth::attempt(['email' => $request->email, 'password' => $request->password]);
-            $request->session()->regenerate();
+            $data = [];
+            UsuarioAssinatura::create(["plano_id" => 1, "user_id" => $user->id, "ativo" => 1, "data_assinatura" => date("Y-m-d"), "data_renovacao" => $dataFuturaFormatada]);
 
+            $data['token'] = $user->createToken('api-token')->plainTextToken;
             $vetor  = User::leftJoin('empresas', 'empresas.id', '=', 'users.empresa_id')->leftJoin('planos', 'planos.id', '=', 'empresas.plano_id')
-            ->where('users.id',Auth::id())->select(['users.*', 'empresas.razao_social', 'empresas.plano_id', 'empresas.segmento_id', 'empresas.situacao', 'planos.recursos'])->first();
+                ->where('users.id',Auth::id())->select(['users.*', 'empresas.razao_social', 'empresas.plano_id', 'empresas.segmento_id', 'empresas.situacao', 'planos.recursos'])->first();
             $data = $vetor;
             $data['recursos'] = json_decode( $data['recursos'] , true);
-            $data['receita'] = fluxo_caixa::getAllMoney(Auth::id());
-            $data['token'] =   csrf_token();
-
+            $data['receita'] = fluxo_caixa::getAllMoney();
+            $data['token_expiracao'] = now()->addMinutes(config('sanctum.expiration'));
+            $data["assinatura"] = UsuarioAssinatura::query()->where("user_id", $user->id)->where("ativo", 1)->first();
             return response()->json($vetor, 200);
 
         }

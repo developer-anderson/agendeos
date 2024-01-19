418654dd365dc88b02b30330012b9df56852c359
Anderson Nascimento <anderson.asnascimentto@gmail.com>
2024-01-19 17:31:16 -0300

    Ajustes

--- app/Models/fluxo_caixa.php
+++ app/Models/fluxo_caixa.php
@@ -16,8 +16,8 @@ class fluxo_caixa extends Model
     {
         $startOfDay = Carbon::parse(date("Y-m-d"))->startOfDay();
         $endOfDay = Carbon::parse(date("Y-m-d"))->endOfDay();
-        $receita = fluxo_caixa::where('tipo_id', 1)->where('user_id', Auth::id())->whereBetween('data', [$startOfDay, $endOfDay])->sum('valor');
-        $despesa = fluxo_caixa::where('tipo_id', 2)->where('user_id', Auth::id())->whereBetween('data', [$startOfDay, $endOfDay])->sum('valor');
+        $receita = fluxo_caixa::where('tipo_id', 1)->where('pagamento_id', 1)->where('user_id', Auth::id())->whereBetween('data', [$startOfDay, $endOfDay])->sum('valor');
+        $despesa = fluxo_caixa::where('tipo_id', 2)->where('pagamento_id', 1)->where('user_id', Auth::id())->whereBetween('data', [$startOfDay, $endOfDay])->sum('valor');
         return $receita-$despesa;
     }
 }

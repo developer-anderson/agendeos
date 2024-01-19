57c57d03d2988e50244cb455f819322ab6c5ea73
Anderson Nascimento <anderson.asnascimentto@gmail.com>
2024-01-19 05:51:15 -0300

    Ajustes

--- app/Http/Controllers/FuncionariosController.php
+++ app/Http/Controllers/FuncionariosController.php
@@ -34,7 +34,7 @@ class FuncionariosController extends Controller
 
         $result = $query->orderBy('id', 'desc')->paginate();
         foreach ($result as $item){
-            $item->dados_conta_acesso = User::query()->where('funcionario_id', $item->id)->select('id', 'nome', 'email')->first();
+            $item->dados_conta_acesso = User::query()->where('funcionario_id', $item->id)->select('id', 'name', 'email')->first();
         }
         return response()->json($result, 200);
 

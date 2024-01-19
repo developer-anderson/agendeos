d83ff428074a1980ea08a00764e2eb5729f2c5c9
Anderson Nascimento <anderson.asnascimentto@gmail.com>
2024-01-19 17:37:16 -0300

    Ajustes

--- app/Models/fluxo_caixa.php
+++ app/Models/fluxo_caixa.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
 class fluxo_caixa extends Model
 {
     use HasFactory;
@@ -16,7 +18,8 @@ class fluxo_caixa extends Model
     {
         $startOfDay = Carbon::parse(date("Y-m-d"))->startOfDay();
         $endOfDay = Carbon::parse(date("Y-m-d"))->endOfDay();
-        $receita = fluxo_caixa::where('tipo_id', 1)->where('pagamento_id', 1)->where('user_id', Auth::id())->whereBetween('data', [$startOfDay, $endOfDay])->sum('valor');
+        $receita = fluxo_caixa::where('tipo_id', 1)->where('pagamento_id', 1)->where('user_id', Auth::id())
+            ->whereBetween('data', [$startOfDay, $endOfDay])->sum(DB::raw('valor - desconto'));
         $despesa = fluxo_caixa::where('tipo_id', 2)->where('pagamento_id', 1)->where('user_id', Auth::id())->whereBetween('data', [$startOfDay, $endOfDay])->sum('valor');
         return $receita-$despesa;
     }

f9516bdf48f6d6babe49e10d5fbfd38e09192417
Anderson Nascimento <anderson.asnascimentto@gmail.com>
2024-02-25 19:14:31 -0300

    ajustes

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -57,6 +57,7 @@ class LoginController extends Controller
         ]);
     }
     public function formatarHorariosFuncionamento($empresas){
+        logger($empresas);
         $data = [
             "domingo" => [
                 "disponivel" => $empresas->domingo,
